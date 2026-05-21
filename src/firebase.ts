cb13c9d27289fb52d28b0e405fa0d240b654d738
Sanke225 <scheickissa@gmail.com>
2026-05-21 09:51:56 +0000

    Erreur de connxion google mail via firebase 3

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,13 +5,13 @@ import { getDatabase } from "firebase/database"
 import { getStorage } from "firebase/storage"
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyAwYkjQiLO9FgzVx9vfFMAQyWYhTYKVIyI",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "e-budget-77ab0.firebaseapp.com",
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL || "https://e-budget-77ab0-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "e-budget-77ab0",
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "e-budget-77ab0.firebasestorage.app",
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "19809884713",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:19809884713:web:0b59a14a507c5aacbd1a38"
 };
 
 // Initialize Firebase
